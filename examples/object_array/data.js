dc6d1e9d7dbc2edfbe3893904a8dcfa511ed5caf
bolash-ju <bolashju@msu.edu>
2025-10-22 09:03:23 -0400

    Added people.js

--- examples/object_array/data.js
+++ examples/object_array/data.js
@@ -1,4 +1,32 @@
 const people = [
+{
+  firstName: "Kathryn",
+  lastName: "Houghton",
+  gradYear: "many moons ago",
+  age: 40,
+  siblings: true,
+  pets: true,
+  favoriteColor: "purple"
+}
 
-	
-  ]
+{
+    firstName: "Julia",
+    lastName: "Bolash",
+    msuGradYear: 2028,
+    age: 19,
+    siblings: true,
+    pets: true,
+    favoriteColor: "purple",
+    closeSecondFavoriteColor: "green"
+}
+
+{
+    firstName: "Thomas",
+    lastName: "Quirin",
+    gradYear: 2026,
+    age: 23,
+    siblings: true,
+    pets: true,
+    favoriteColor: "light-blue",
+}	
+]
