c05045ce23830816ff0dead677323c82796886e1
kathrynth <166564274+kathrynth@users.noreply.github.com>
2026-03-09 09:54:37 -0400

    Update data.js
    updated to fix smart quotes

--- examples/object_array/data.js
+++ examples/object_array/data.js
@@ -10,24 +10,24 @@ const people = [
 },
 
 {
-  firstName: “Isabella”,
-  lastName: “Jungman”,
-  gradYear: “2027”,
+  firstName: "Isabella",
+  lastName: "Jungman",
+  gradYear: "2027",
   age: 21,
   siblings: true,
   pets: false,
-  favoriteColor: “Pink”
+  favoriteColor: "Pink"
 },
 
 
 {
-  firstName: “Alison”,
-  lastName: “Bamberg”,
-  gradYear: “2028”,
+  firstName: "Alison",
+  lastName: "Bamberg",
+  gradYear: "2028",
   age: 21,
   siblings: true,
   pets: true,
-  favoriteColor: “Pink”
+  favoriteColor: "Pink"
 },
 
 {
